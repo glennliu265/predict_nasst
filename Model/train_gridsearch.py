3f36fc92e123ce79ca27b2e69848aed9ae828a7d
Glenn Liu <glenn.y.liu@gmail.com>
2023-11-06 19:17:22 -0500

    Train 2 4 6 with dropout 0.5

--- Model/train_gridsearch.py
+++ Model/train_gridsearch.py
@@ -136,7 +136,7 @@ varname    = varnames[0]
 predictors = data[[0],...] # Get selected predictor
 
 # For FNN4, just test the number of layers and units
-nlayers  = [12,14,16]      # Number of Layers
+nlayers  = [2,4,6]      # Number of Layers
 nunits   = [128,] # Number of Units
 #dropouts = [0.5,0] # Useing Dropout Layer 
 
